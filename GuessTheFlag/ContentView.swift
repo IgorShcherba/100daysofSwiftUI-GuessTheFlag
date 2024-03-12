03b259290fb343ffb636b56d77daa7ac715ba1c6
Igor Shcherba <igor.shcherba@callstack.com>
2024-03-12 23:03:10 +0100

    using Buttons

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -10,12 +10,37 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Basic") {}
+            Button("Bordered") {}
+                .buttonStyle(.bordered)
+
+            Button("Destrtive bordered", role: .destructive) {}
+                .buttonStyle(.bordered)
+
+            Button("Prominent") {}
+                .buttonStyle(.borderedProminent)
+                .tint(.mint)
+
+            Button("Prominent destructive", role: .destructive) {}
+                .buttonStyle(.borderedProminent)
+
+            Button {} label: {
+                Text("Tap me!")
+                    .padding()
+                    .foregroundStyle(.white)
+                    .background(.red)
+            }
+            .buttonStyle(.bordered)
+
+            Button {} label: {
+                Label("Edit", systemImage: "pencil")
+                    .padding()
+                    .foregroundStyle(.white)
+                    .background(.blue)
+            }
+
+            Button("Button", systemImage: "pencil") {}
         }
-        .padding()
     }
 }
 
