549c829ca0cca4564c5d814619db661c390d9a74
Igor Shcherba <igor.shcherba@callstack.com>
2024-04-18 09:55:38 +0200

    feat: final design of the app

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,40 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+
+    var correctAnswer = Int.random(in: 0 ... 2)
     var body: some View {
-        VStack {
-            Button("Basic") {}
-            Button("Bordered") {}
-                .buttonStyle(.bordered)
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+            ], center: .top, startRadius: 200, endRadius: 700)
+                .ignoresSafeArea()
+            VStack {
+                Spacer()
 
-            Button("Destrtive bordered", role: .destructive) {}
-                .buttonStyle(.bordered)
-
-            Button("Prominent") {}
-                .buttonStyle(.borderedProminent)
-                .tint(.mint)
-
-            Button("Prominent destructive", role: .destructive) {}
-                .buttonStyle(.borderedProminent)
-
-            Button {} label: {
-                Text("Tap me!")
-                    .padding()
+                Text("Guess the flag")
+                    .font(.largeTitle.bold())
                     .foregroundStyle(.white)
-                    .background(.red)
-            }
-            .buttonStyle(.bordered)
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.semibold))
+                        Text(countries[correctAnswer])
 
-            Button {} label: {
-                Label("Edit", systemImage: "pencil")
-                    .padding()
+                            .font(.largeTitle.weight(.heavy))
+                    }
+
+                    ForEach(0 ..< 3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .clipShape(.capsule)
+                                .shadow(radius: 5)
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+
+                Spacer()
+
+                Text("Score ???")
                     .foregroundStyle(.white)
-                    .background(.blue)
-            }
+                    .font(.title.bold())
 
-            Button("Button", systemImage: "pencil") {}
+                Spacer()
+            }
+            .padding()
+        }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is ???")
         }
     }
+
+    func flagTapped(_ number: Int) {
+        if correctAnswer == number {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+
+        showingScore = true
+    }
+
+    func askQuestion() {}
 }
 
 #Preview {
